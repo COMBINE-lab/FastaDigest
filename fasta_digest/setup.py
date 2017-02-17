d357891a5426f03b9b7c81180ec60ea64d65a874
Rob Patro <rob@newton>
2017-02-17 16:54:24 -0500

    make script executable

---
+++ fasta_digest/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='fasta_digest',
+    version='0.1',
+    description='Get the signature of a fasta file',
+    scripts=['bin/compute_fasta_digest'],
+    url='http://github.com/COMBINE-lab/FastaDigest',
+    author='Rob Patro',
+    license='BSD',
+    packages=['fasta_digest'],
+    install_requires=[
+        'coloredlogs'
+    ],
+    zip_safe=False)
